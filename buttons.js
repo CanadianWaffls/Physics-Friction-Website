5b7c96b28ae3147ceee9907eac07ced89a60ad3c
CanadianWaffls <Tsagfuller@gmail.com>
2024-05-26 21:56:51 -0400

    Finished

--- buttons.js
+++ buttons.js
@@ -1,3 +1,15 @@
-function button(){
-    alert("This button works")
+var e = 1;
+function exampleButton(){
+    if(e == 1){
+        e = 2;
+        document.getElementById("exampleButton").textContent = "Add Friction";
+        document.getElementById("frictionExampleImg").src = "friction2.jpg";
+        document.getElementById("frictionExampleText").textContent = "Without friction the tire spins but does not push against the ground meaning the car moves forward very little if at all."
+    }
+    else{
+        e = 1;
+        document.getElementById("exampleButton").textContent = "Remove Friction";
+        document.getElementById("frictionExampleImg").src = "friction1.jpg";
+        document.getElementById("frictionExampleText").textContent = "With Friction the tire turns and pushes off the ground, moving the car forward."
+    }
 }
